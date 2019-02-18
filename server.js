bcc4d9f3a297dbe68267ec1d6a07d4d7f0aee55e
Bo <clouculum@gmail.com>
2019-02-17 21:58:20 -0700

    update

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+const { DATABASE_URL, PORT } = require('./config');
 
 // log the http layer
 app.use(morgan('common'));
 
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
+
+app.use('/login', usersRouter);
+app.use('/events', eventRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
