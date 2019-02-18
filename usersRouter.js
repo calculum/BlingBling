f5395469973fc5208ea4942212fa154c1ee5f05e
Bo <clouculum@gmail.com>
2019-02-17 23:21:21 -0700

    update

--- usersRouter.js
+++ usersRouter.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const router = express.Router();
 
 // Load User Model
-require('../models/User');
+require('../models/users');
 const User = mongoose.model('users');
 
 // User Login Route
