17eb2de044f58bbb36c235d1d6c2c2a0b12e9959
bo <clouculum@gmail.com>
2019-03-04 21:11:47 -0700

    update

--- config.js
+++ config.js
@@ -3,7 +3,5 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   'mongodb://dbuser:dbpass1@ds113815.mlab.com:13815/cid-with-mongoose';
-exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL ||
-  '#';
+
 exports.PORT = process.env.PORT || 5000;
