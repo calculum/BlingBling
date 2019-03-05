da86a99dec42cc55beaedf3a1c537aaef2eed895
bo <clouculum@gmail.com>
2019-03-04 22:16:55 -0700

    update

--- blingRouter.js
+++ blingRouter.js
@@ -5,7 +5,7 @@ const { ensureAuthenticated } = require('./Authenticate/auth');
 
 
 // Load Models
-require('./models/Bling');
+require('../BlingBling/models/Bling');
 const Bling = mongoose.model('blings');
 
 // Bling index page
